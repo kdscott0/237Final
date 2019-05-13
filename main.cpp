84d780bbf1636a44e96e1f0fde045ae24c4cda56
NWalker4483 <niwal7@morgan.edu>
2019-05-12 22:49:50 -0400

    Sync

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include "User.cpp"
 using namespace std;
 void ShowMainMenu();
-inline bool file_exists(const string& name) {
+bool file_exists(string& name) {
     ifstream f(name.c_str());
     return f.good();
 }
